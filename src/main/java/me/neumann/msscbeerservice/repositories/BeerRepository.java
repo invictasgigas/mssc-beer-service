1b0b6df98a845062786d0a192b6b514a100c6519
Sebastian <sebastianneumann@hotmail.com>
2020-06-04 22:23:50 +0200

    adding beer repository. Closes #6

---
+++ src/main/java/me/neumann/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package me.neumann.msscbeerservice.repositories;
+
+import me.neumann.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
