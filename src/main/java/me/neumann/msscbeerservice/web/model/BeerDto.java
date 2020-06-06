2d922b2c02b99140dbf1f779d1c2283416c072c3
Sebastian <sebastianneumann@hotmail.com>
2020-06-06 23:39:29 +0200

    adding jackson formats to BeerDto. Closes #13

--- src/main/java/me/neumann/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/me/neumann/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package me.neumann.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,9 +25,13 @@ public class BeerDto {
     @Null
     private Integer version;
 
+
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -38,9 +43,11 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 
 }
